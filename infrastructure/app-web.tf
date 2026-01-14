88bb7c9b01896cd967b7c175d8c5a56b6ebf6bda
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-01-14 12:52:35 +0000

    fix(infrastructure): disabling auth to test

--- infrastructure/app-web.tf
+++ infrastructure/app-web.tf
@@ -44,7 +44,7 @@ module "template_app_web" {
     #checkov:skip=CKV_SECRET_6: "Secret is securely stored in Key Vault"
     auth_provider_secret = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
     auth_tenant_endpoint = "https://login.microsoftonline.com/${data.azurerm_client_config.current.tenant_id}/v2.0"
-    allowed_applications = var.auth_config.application_id
+    allowed_applications = var.auth_config.applications_id
     allowed_audiences    = "https://${var.web_app_domain}/.auth/login/aad/callback"
     excluded_paths       = []
   }
