84418529f009a542ee1b67debb0ecf83c451d9a8
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-02-02 14:55:40 +0000

    feat(infra): testing metric alert for log analytics

--- infrastructure/front-door-premium.tf
+++ infrastructure/front-door-premium.tf
@@ -1,18 +1,18 @@
-resource "azurerm_cdn_frontdoor_origin_group" "web" {
-  name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
-  cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
-  session_affinity_enabled = true
-
-  health_probe {
-    interval_in_seconds = 240
-    path                = "/"
-    protocol            = "Https"
-    request_type        = "HEAD"
-  }
-
-  load_balancing {
-    additional_latency_in_milliseconds = 0
-    sample_size                        = 16
-    successful_samples_required        = 3
-  }
-}
+# resource "azurerm_cdn_frontdoor_origin_group" "web" {
+#   name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
+#   cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
+#   session_affinity_enabled = true
+#
+#   health_probe {
+#     interval_in_seconds = 240
+#     path                = "/"
+#     protocol            = "Https"
+#     request_type        = "HEAD"
+#   }
+#
+#   load_balancing {
+#     additional_latency_in_milliseconds = 0
+#     sample_size                        = 16
+#     successful_samples_required        = 3
+#   }
+# }
