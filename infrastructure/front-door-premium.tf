3471552068edc3dcbe8d1b88d7468ea58ef08bc5
Snasim21 <Sabbir.Nasim.47@planninginspectorate.gov.uk>
2026-02-17 14:53:28 +0000

    chore: format front-door-premium.tf with terraform fmt

--- infrastructure/front-door-premium.tf
+++ infrastructure/front-door-premium.tf
@@ -23,7 +23,7 @@ resource "azurerm_cdn_frontdoor_origin" "web_app" {
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
   enabled                       = true
 
-  provider                      = azurerm.front_door
+  provider = azurerm.front_door
 
   host_name                      = module.template_app_web.default_site_hostname
   origin_host_header             = module.template_app_web.default_site_hostname
