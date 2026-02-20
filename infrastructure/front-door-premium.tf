f3ab493b86fa01b6852a3ddb17601e612abbd6d5
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-02-20 13:12:23 +0000

    fix(infra): inputting resource blocks for FD

--- infrastructure/front-door-premium.tf
+++ infrastructure/front-door-premium.tf
@@ -1,8 +1,8 @@
 resource "azurerm_cdn_frontdoor_origin_group" "web" {
   name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
   cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
-  session_affinity_enabled = true
   provider                 = azurerm.front_door
+  session_affinity_enabled = true
 
   health_probe {
     interval_in_seconds = 240
@@ -18,37 +18,55 @@ resource "azurerm_cdn_frontdoor_origin_group" "web" {
   }
 }
 
-import {
-  id = "/subscriptions/edb1ff78-90da-4901-a497-7e79f966f8e2/resourceGroups/pins-rg-common-tooling/providers/Microsoft.Cdn/profiles/pins-fd-common-tooling/originGroups/pins-fd-template-web-test/origins/pins-fd-template-origin-test"
-  to = azurerm_cdn_frontdoor_origin.web_app
-}
+resource "azurerm_cdn_frontdoor_origin" "web" {
+  name                           = "${local.org}-fd-${local.service_name}-web-${var.environment}"
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.web.id
+  enabled                        = true
+  certificate_name_check_enabled = true
 
-resource "azurerm_cdn_frontdoor_origin" "web_app" {
-  name                          = "${local.org}-fd-${local.service_name}-origin-${var.environment}"
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
-  enabled                       = true
+  host_name          = module.template_app_web.default_site_hostname
+  origin_host_header = module.template_app_web.default_site_hostname
+  http_port          = 80
+  https_port         = 443
+  priority           = 1
+  weight             = 1000
 
   provider = azurerm.front_door
-
-  host_name                      = module.template_app_web.default_site_hostname
-  origin_host_header             = module.template_app_web.default_site_hostname
-  http_port                      = 80
-  https_port                     = 443
-  priority                       = 1
-  weight                         = 100
-  certificate_name_check_enabled = true
 }
 
 resource "azurerm_cdn_frontdoor_route" "web" {
   name                          = "${local.org}-fd-${local.service_name}-web-${var.environment}"
   cdn_frontdoor_endpoint_id     = data.azurerm_cdn_frontdoor_endpoint.web.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
-  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.web_app.id]
-  patterns_to_match             = ["/*"]
-  supported_protocols           = ["Http", "Https"]
-  https_redirect_enabled        = true
-  forwarding_protocol           = "MatchRequest"
-  link_to_default_domain        = true
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.web.id]
+
+  forwarding_protocol    = "MatchRequest"
+  https_redirect_enabled = true
+  patterns_to_match      = ["/*"]
+  supported_protocols    = ["Http", "Https"]
+
+  cdn_frontdoor_custom_domain_ids = [azurerm_cdn_frontdoor_custom_domain.web.id]
+  link_to_default_domain          = false
 
   provider = azurerm.front_door
 }
+
+resource "azurerm_cdn_frontdoor_custom_domain" "web" {
+  name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
+  cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
+  host_name                = var.web_app_domain
+
+  tls {
+    certificate_type = "ManagedCertificate"
+  }
+
+  provider = azurerm.front_door
+}
+
+resource "azurerm_cdn_frontdoor_custom_domain_association" "web" {
+  cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.web.id
+  cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.web.id]
+
+  provider = azurerm.front_door
+}
+
