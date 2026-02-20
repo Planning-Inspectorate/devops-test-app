d73f9bfe8827bff0433837dd5706c60a1c72f89c
Snasim21 <Sabbir.Nasim.47@planninginspectorate.gov.uk>
2026-02-20 16:12:56 +0000

    feat: add firewall policy and security policy resources

--- infrastructure/front-door-premium.tf
+++ infrastructure/front-door-premium.tf
@@ -18,20 +18,18 @@ resource "azurerm_cdn_frontdoor_origin_group" "web" {
   }
 }
 
-resource "azurerm_cdn_frontdoor_origin" "web" {
-  name                           = "${local.org}-fd-${local.service_name}-web-${var.environment}"
+resource "azurerm_cdn_frontdoor_origin" "web_app" {
+  name                           = "${local.org}-fd-${local.service_name}-origin-${var.environment}"
   cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.web.id
   enabled                        = true
+  provider                       = azurerm.front_door
+  host_name                      = module.template_app_web.default_site_hostname
+  origin_host_header             = module.template_app_web.default_site_hostname
+  http_port                      = 80
+  https_port                     = 443
+  priority                       = 1
+  weight                         = 100
   certificate_name_check_enabled = true
-
-  host_name          = module.template_app_web.default_site_hostname
-  origin_host_header = module.template_app_web.default_site_hostname
-  http_port          = 80
-  https_port         = 443
-  priority           = 1
-  weight             = 1000
-
-  provider = azurerm.front_door
 }
 
 resource "azurerm_cdn_frontdoor_route" "web" {
@@ -39,17 +37,16 @@ resource "azurerm_cdn_frontdoor_route" "web" {
   cdn_frontdoor_endpoint_id     = data.azurerm_cdn_frontdoor_endpoint.web.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.web.id]
-
-  forwarding_protocol    = "MatchRequest"
-  https_redirect_enabled = true
-  patterns_to_match      = ["/*"]
-  supported_protocols    = ["Http", "Https"]
-
-  cdn_frontdoor_custom_domain_ids = [azurerm_cdn_frontdoor_custom_domain.web.id]
-  link_to_default_domain          = false
+  patterns_to_match             = ["/*"]
+  supported_protocols           = ["Http", "Https"]
+  https_redirect_enabled        = true
+  forwarding_protocol           = "MatchRequest"
+  link_to_default_domain        = true
 
   provider = azurerm.front_door
+
 }
+
 
 resource "azurerm_cdn_frontdoor_custom_domain" "web" {
   name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
@@ -59,14 +56,57 @@ resource "azurerm_cdn_frontdoor_custom_domain" "web" {
   tls {
     certificate_type = "ManagedCertificate"
   }
+  provider = azurerm.front_door
+}
+
+
+resource "azurerm_cdn_frontdoor_firewall_policy" "web" {
+  name                              = replace("${local.org}-fd-${local.service_name}-waf-${var.environment}", "-", "")
+  resource_group_name               = var.tooling_config.frontdoor_rg
+  sku_name                          = data.azurerm_cdn_frontdoor_profile.web.sku_name
+  enabled                           = true
+  mode                              = "Prevention"
+  custom_block_response_status_code = 403
+
+  custom_rule {
+    name     = "BlockLocalIPs"
+    enabled  = true
+    priority = 1
+    type     = "MatchRule"
+    action   = "Block"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["10.0.1.0/24", "10.0.0.0/24"]
+    }
+  }
+
+  managed_rule {
+    type    = "DefaultRuleSet"
+    version = "1.0"
+    action  = "Log"
+  }
 
   provider = azurerm.front_door
 }
 
-resource "azurerm_cdn_frontdoor_custom_domain_association" "web" {
-  cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.web.id
-  cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.web.id]
+resource "azurerm_cdn_frontdoor_security_policy" "web" {
+  name                     = replace("${local.org}-fd-${local.service_name}-security-policy-${var.environment}", "-", "")
+  cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
 
+  security_policies {
+    firewall {
+      cdn_frontdoor_firewall_policy_id = azurerm_cdn_frontdoor_firewall_policy.web.id
+
+      association {
+        domain {
+          cdn_frontdoor_domain_id = azurerm_cdn_frontdoor_custom_domain.web.id
+        }
+        patterns_to_match = ["/*"]
+      }
+    }
+  }
   provider = azurerm.front_door
 }
-
