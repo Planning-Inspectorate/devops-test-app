ac8d904ab845a94363c7885e8ec3e1d6e1b1107d
Snasim21 <Sabbir.Nasim.47@planninginspectorate.gov.uk>
2026-02-23 10:03:03 +0000

    fix: revert unrelated resource changes, keep only firewall additions

--- infrastructure/front-door-premium.tf
+++ infrastructure/front-door-premium.tf
@@ -18,33 +18,32 @@ resource "azurerm_cdn_frontdoor_origin_group" "web" {
   }
 }
 
-resource "azurerm_cdn_frontdoor_origin" "web_app" {
-  name                           = "${local.org}-fd-${local.service_name}-origin-${var.environment}"
+resource "azurerm_cdn_frontdoor_origin" "web" {
+  name                           = "${local.org}-fd-${local.service_name}-web-${var.environment}"
   cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.web.id
   enabled                        = true
-  provider                       = azurerm.front_door
   host_name                      = module.template_app_web.default_site_hostname
   origin_host_header             = module.template_app_web.default_site_hostname
   http_port                      = 80
   https_port                     = 443
   priority                       = 1
-  weight                         = 100
+  weight                         = 1000
   certificate_name_check_enabled = true
+  provider = azurerm.front_door
 }
 
 resource "azurerm_cdn_frontdoor_route" "web" {
   name                          = "${local.org}-fd-${local.service_name}-web-${var.environment}"
   cdn_frontdoor_endpoint_id     = data.azurerm_cdn_frontdoor_endpoint.web.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
-  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.web_app.id]
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.web.id]
+  forwarding_protocol           = "MatchRequest"
+  https_redirect_enabled        = true
   patterns_to_match             = ["/*"]
   supported_protocols           = ["Http", "Https"]
-  https_redirect_enabled        = true
-  forwarding_protocol           = "MatchRequest"
-  link_to_default_domain        = true
-
+  cdn_frontdoor_custom_domain_ids = [azurerm_cdn_frontdoor_custom_domain.web.id]
+  link_to_default_domain        = false
   provider = azurerm.front_door
-
 }
 
 
