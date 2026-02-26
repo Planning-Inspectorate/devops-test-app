67015896aecf9d9edad7ff24c3b91afeeadcb85a
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-02-26 14:10:14 +0000

    fix: testing pes and if pipeline goes through first time

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,16 +17,38 @@ resource "azurerm_key_vault" "main" {
   #checkov:skip=CKV_AZURE_109: TODO: consider firewall settings, route traffic via VNet
   #checkov:skip=CKV_AZURE_189: "Ensure that Azure Key Vault disables public network access"
   #checkov:skip=CKV2_AZURE_32: "Ensure private endpoint is configured to key vault"
-  name                        = format("%s-kv-%s", local.org, local.resource_suffix)
-  location                    = module.primary_region.location
-  resource_group_name         = azurerm_resource_group.primary.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-  rbac_authorization_enabled  = true
+  name                          = format("%s-kv-%s", local.org, local.resource_suffix)
+  location                      = module.primary_region.location
+  resource_group_name           = azurerm_resource_group.primary.name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = true
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = false
 
   sku_name = "standard"
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "kv_primary" {
+  name                = "${local.org}-pe-${local.service_name}-kv-${var.environment}"
+  resource_group_name = azurerm_resource_group.primary.name
+  location            = module.primary_region.location
+  subnet_id           = azurerm_subnet.main.id
+
+  private_dns_zone_group {
+    name                 = "keyvaultprivatednszone"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.keyvault.id]
+  }
+
+  private_service_connection {
+    name                           = "privateendpointconnection"
+    private_connection_resource_id = azurerm_key_vault.main.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
 
   tags = local.tags
 }
@@ -42,7 +64,7 @@ resource "azurerm_key_vault_access_policy" "admins" {
   storage_permissions     = ["Get", "List", "Set"]
 }
 
-# # secrets to be manually populated
+# secrets to be manually populated
 resource "azurerm_key_vault_secret" "manual_secrets" {
   #checkov:skip=CKV_AZURE_41: expiration not valid
   for_each = toset(local.secrets)
@@ -52,11 +74,13 @@ resource "azurerm_key_vault_secret" "manual_secrets" {
   value        = "<terraform_placeholder>"
   content_type = "plaintext"
 
-  tags = local.tags
-
   lifecycle {
     ignore_changes = [
       value
     ]
   }
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.keyvault]
+
+  tags = local.tags
 }
